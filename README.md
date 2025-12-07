423c7121dce64b82911d5218a887611ba63078e5
kyserd <kyserdy@gmail.com>
2025-12-06 21:12:25 -0500

    bootstrap script changes and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# nix home-manager example
+
+This aims to be a simple example on how to bootstrap nix/home-manager on a foreign distro.
+
+Based on [this amazing guide](https://github.com/Evertras/simple-homemanager) by Evertras
+
+## Steps:
+
+1. Install Nix (you may need to install curl)
+```
+sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
+
+````
+
+2. Open a new shell and run:
+```
+./scripts/bootstrap.sh
+```
+
+This will install home-manager, link the dotfiles in `.config` to `~/.config` and preform a home build/switch.
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,13 +2,13 @@
 
 SCRIPT_DIR=$(dirname "$0")
 
-# Install nix
-sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
-
 # Install home-manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz home-manager
 nix-channel --update
 
+# Run the Home Manager installation command and create the first Home Manager generation
+nix-shell '<home-manager>' -A install
+
 source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 
-nix-shell -p stow just -- just --justfile ${SCRIPT_DIR}/../justfile stow home-switch 
+nix-shell -p stow just --run "just --justfile ${SCRIPT_DIR}/../justfile stow home-switch"
